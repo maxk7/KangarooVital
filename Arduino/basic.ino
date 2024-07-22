be150e3b322842c7d6fa04424ace3097ecf2cbca
Max Konzerowsky <43018603+maxk7@users.noreply.github.com>
2024-07-22 15:04:13 -0400

    Update basic.ino

--- Arduino/basic.ino
+++ Arduino/basic.ino
@@ -1,54 +1,46 @@
 // ********* basic.ino **********/
-// Interfacing template for our arduino circuit
+// Interfacing template for our Arduino circuit
 // By Max Konzerowsky
 // July 2024
 //
 // Basic UI: Input from `potPin` outputted to `ledPin` voltage
-// Basic HR: 250mv outputted to `pmwPin` every `interval` milliseconds
-
+// Basic HR: 250mv outputted to `pwmPin` every `interval` milliseconds
 
 /********** variables **********/
 // pins
 const int potPin = A0;              // Define the analog pin for the potentiometer
 const int ledPin = 9;               // Define the digital pin for the LED
-const int pwmPin = 3;               // Define the pin for pulse width modulation to heart beat
+const int pwmPin = 3;               // Define the pin for pulse width modulation for heartbeat
 
-// potentiometer and led values
+// potentiometer and LED values
 int potValue = 0;
 int ledValue = 0;
 
-// hr & bpm
-unsigned long previousMillis = 0;   // will store last time the output was updated
-const long interval = 1000;         // interval at which to send 250mV (milliseconds)
+// HR & BPM
+unsigned long previousMillis = 0;   // Will store last time the output was updated
 
 // pulse
-const int operatingVoltage = 5000;  // in millivolts
+const int operatingVoltage = 5000;  // Operating voltage in millivolts
 bool pulseActive = false;
-
 
 /********** setup **********/
 void setup() {
   pinMode(ledPin, OUTPUT);    // Initialize the LED pin as an output
-  pinMode(pwmPin, OUTPUT);    // Initialize the pmw pin as an output
-  analogWrite(pwmPin, 0);     // 0V output to haptic motor
+  pinMode(pwmPin, OUTPUT);    // Initialize the PWM pin as an output
+  analogWrite(pwmPin, 0);     // Set initial output to 0V for haptic motor
 
   // Initialize the serial communication for debugging
   Serial.begin(9600);
 }
 
-
 /********** main loop **********/
 void loop() {
-  potValue = analogRead(potPin);              // Read the potentiometer value (0-1023)
-  ledValue = map(potValue, 0, 1023, 0, 255);  // Map the potentiometer value to a PWM value (0-255)
-  analogWrite(ledPin, ledValue);              // Output the PWM value to the LED
-
-  // pulse hardcoded for testing (75 bpm, 20ms, 250mv)
+  // Pulse hardcoded for testing (75 bpm, 20ms, 250mV)
   int bpm = 75;                               // Beats per minute
   int targetMillivolts = 250;                 // Output voltage in millivolts
   long pulseDuration = 20;                    // Duration of the pulse in milliseconds
 
-  // helper function execution
+  // Helper function execution
   pulse(targetMillivolts, pulseDuration, bpm);
   nonHRInterface();
 
@@ -56,10 +48,13 @@ void loop() {
   delay(10);
 }
 
-
 /********** helper functions **********/
-// basic UI for potentiometer and input
+// Basic UI for potentiometer and LED input
 void nonHRInterface() {
+  potValue = analogRead(potPin);              // Read the potentiometer value (0-1023)
+  ledValue = map(potValue, 0, 1023, 0, 255);  // Map the potentiometer value to a PWM value (0-255)
+  analogWrite(ledPin, ledValue);              // Output the PWM value to the LED
+  
   // Print the potentiometer and LED values to the serial monitor
   Serial.print("Potentiometer Value: ");
   Serial.print(potValue);
@@ -67,8 +62,7 @@ void nonHRInterface() {
   Serial.println(ledValue);
 }
 
-
-// pulse functionality
+// Pulse functionality
 void pulse(int targetMillivolts, long pulseDuration, int bpm) {
   // Calculate the interval based on the BPM
   long calculatedInterval = 60000 / bpm;
@@ -97,8 +91,7 @@ void pulse(int targetMillivolts, long pulseDuration, int bpm) {
   }
 }
 
-
-// return dutyCycle intefer from specified millivolts
+// Return duty cycle inferred from specified millivolts
 int calculateDutyCycle(int millivolts, int operatingVoltage = 5000) {
   // Ensure the millivolts value is within the range
   if (millivolts < 0) {
@@ -111,4 +104,3 @@ int calculateDutyCycle(int millivolts, int operatingVoltage = 5000) {
   int dutyCycle = (millivolts * 255) / operatingVoltage;
   return dutyCycle;
 }
-
